7eb71e0959745d1f309ea53dae7e80ca2b0634bf
SteveScorfield <steve.scorfield@ons.gov.uk>
2020-09-24 11:43:03 +0100

    Fixed lastActionInstruction null pointer exception

--- src/main/java/uk/gov/ons/census/fwmt/jobservice/transition/utils/RetrieveTransitionRules.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/transition/utils/RetrieveTransitionRules.java
@@ -33,8 +33,8 @@ public class RetrieveTransitionRules {
       cacheType = "EMPTY";
       recordAge = "NEWER";
     } else {
-      String lastActionType = cache.lastActionInstruction;
-      if(!lastActionType.isEmpty()) {
+      String lastActionType = cache.getLastActionInstruction();
+      if(lastActionType != null && !lastActionType.isEmpty()) {
         cacheType = cache.lastActionInstruction;
       } else {
         cacheType = "EMPTY";

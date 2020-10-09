553e796ca0064381a5bdbc805aafd810af374ea8
SteveScorfield <steve.scorfield@ons.gov.uk>
2020-10-09 15:52:06 +0100

    Fixed issue with cancel processor

--- src/main/java/uk/gov/ons/census/fwmt/jobservice/service/converter/hh/HhCancelConverter.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/service/converter/hh/HhCancelConverter.java
@@ -1,14 +1,14 @@
 package uk.gov.ons.census.fwmt.jobservice.service.converter.hh;
 
 import uk.gov.ons.census.fwmt.common.data.tm.CasePauseRequest;
-import uk.gov.ons.census.fwmt.common.rm.dto.FwmtActionInstruction;
+import uk.gov.ons.census.fwmt.common.rm.dto.FwmtCancelActionInstruction;
 
 public final class HhCancelConverter {
 
   private HhCancelConverter(){
   }
 
-  public static CasePauseRequest buildCancel(FwmtActionInstruction ffu) {
+  public static CasePauseRequest buildCancel(FwmtCancelActionInstruction ffu) {
       return CasePauseRequest.builder()
           .code("inf")
           .build();

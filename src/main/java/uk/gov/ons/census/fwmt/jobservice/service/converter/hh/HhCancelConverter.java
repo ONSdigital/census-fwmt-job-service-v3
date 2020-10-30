f1936a216d37475f3b5d3048a7bb7afac1f7e432
SteveScorfield <steve.scorfield@ons.gov.uk>
2020-10-30 14:11:39 +0000

    Added effectiveFrom to HH Cancel

--- src/main/java/uk/gov/ons/census/fwmt/jobservice/service/converter/hh/HhCancelConverter.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/service/converter/hh/HhCancelConverter.java
@@ -3,14 +3,29 @@ package uk.gov.ons.census.fwmt.jobservice.service.converter.hh;
 import uk.gov.ons.census.fwmt.common.data.tm.CasePauseRequest;
 import uk.gov.ons.census.fwmt.common.rm.dto.FwmtCancelActionInstruction;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public final class HhCancelConverter {
 
   private HhCancelConverter(){
   }
 
   public static CasePauseRequest buildCancel(FwmtCancelActionInstruction ffu) {
-      return CasePauseRequest.builder()
+    Date currentDate = null;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy", Locale.ENGLISH);
+    try {
+      currentDate = dateFormat.parse(dateFormat.format(new Date(System.currentTimeMillis())));
+    } catch (ParseException e) {
+      String error = e.toString();
+      System.out.println(error);
+    }
+    
+    return CasePauseRequest.builder()
           .code("inf")
+          .effectiveFrom(currentDate)
           .build();
   }
 }

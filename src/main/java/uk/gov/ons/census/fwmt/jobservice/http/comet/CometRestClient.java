00c9b596907f99ffb7532e59863ea1e825f638de
Ramesh Pydi <ramesh.pydi@ext.ons.gov.uk>
2020-11-19 09:49:49 +0000

    FWMT-2771: RestTemplate http connection pooling and request timeout.

--- src/main/java/uk/gov/ons/census/fwmt/jobservice/http/comet/CometRestClient.java
+++ src/main/java/uk/gov/ons/census/fwmt/jobservice/http/comet/CometRestClient.java
@@ -1,12 +1,8 @@
 package uk.gov.ons.census.fwmt.jobservice.http.comet;
 
-import java.net.MalformedURLException;
-import java.util.Date;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
+import com.microsoft.aad.adal4j.AuthenticationContext;
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.aad.adal4j.ClientCredential;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,11 +10,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import com.microsoft.aad.adal4j.AuthenticationContext;
-import com.microsoft.aad.adal4j.AuthenticationResult;
-import com.microsoft.aad.adal4j.ClientCredential;
-
 import uk.gov.ons.census.fwmt.common.data.modelcase.ModelCase;
 import uk.gov.ons.census.fwmt.common.data.tm.CasePauseRequest;
 import uk.gov.ons.census.fwmt.common.data.tm.CaseRequest;
@@ -27,6 +18,13 @@ import uk.gov.ons.census.fwmt.common.data.tm.ReopenCaseRequest;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
 import uk.gov.ons.census.fwmt.jobservice.config.CometConfig;
+
+import java.net.MalformedURLException;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Component
 public class CometRestClient {
@@ -56,10 +54,11 @@ public class CometRestClient {
   public CometRestClient(
       CometConfig cometConfig,
       RestTemplateBuilder restTemplateBuilder,
-      GatewayEventManager gatewayEventManager) {
+      GatewayEventManager gatewayEventManager,
+      RestTemplate restTemplate) {
     this.cometConfig = cometConfig;
-    this.restTemplate = restTemplateBuilder.errorHandler(new CometRestClientResponseErrorHandler())
-        .basicAuthentication(cometConfig.userName, cometConfig.password).build();
+    this.restTemplate = restTemplate;
+
     this.gatewayEventManager = gatewayEventManager;
     this.cometUrl = cometConfig.baseUrl + cometConfig.caseCreatePath;
     this.auth = null;

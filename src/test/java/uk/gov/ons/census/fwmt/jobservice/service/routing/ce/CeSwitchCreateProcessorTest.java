a67670a1ca4c3595cb046dc13294bc842f81e6d6
conorocallaghan <ocallaghanit@gmail.com>
2021-01-04 22:33:11 +0000

    added basic test - template

--- src/test/java/uk/gov/ons/census/fwmt/jobservice/service/routing/ce/CeSwitchCreateProcessorTest.java
+++ src/test/java/uk/gov/ons/census/fwmt/jobservice/service/routing/ce/CeSwitchCreateProcessorTest.java
@@ -1,10 +1,13 @@
 package uk.gov.ons.census.fwmt.jobservice.service.routing.ce;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.ons.census.fwmt.common.data.tm.SurveyType;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import uk.gov.ons.census.fwmt.common.rm.dto.FwmtActionInstruction;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
@@ -15,7 +18,6 @@ import uk.gov.ons.census.fwmt.jobservice.service.routing.RoutingValidator;
 
 import java.time.Instant;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class CeSwitchCreateProcessorTest {
@@ -43,21 +45,20 @@ public class CeSwitchCreateProcessorTest {
     return GatewayCache.builder().caseId("").build();
   }
 
-  // what are we going to test here -
   @Test
-  public void shouldTriggerErrorEvent() throws GatewayException {
-
-    FwmtActionInstruction instruction = createInstruction();
-    ceSwitchCreateProcessor.process(instruction, createGatewayCache(), Instant.now());
-
-    assertTrue(false);
+  @DisplayName("Should throw Gateway Exception and trigger event for invalid survey type")
+  public void shouldHandleIncorrectSurveyTypeCE() {
+    final FwmtActionInstruction instruction = createInstruction();
+    instruction.setSurveyType(SurveyType.AC);
+    instruction.setCaseId("1234");
+    Assertions.assertThrows(GatewayException.class, () -> {
+      ceSwitchCreateProcessor.process(instruction, createGatewayCache(), Instant.now());
+    });
   }
 
-  // put in all the outcomes - switch - what comet requests are being made
   @Test
-  public void shouldMessagEcomment() {
-    // put in all the outcomes - switch - what comet requests are being made
+  @DisplayName("Should handle type - CE_EST_D ")
+  public void shouldMessageForCE_EST_D() {
 
-    // what things are happening in that switch ?
   }
 }
